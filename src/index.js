332c0cf413742db9bbdef34f12174b7f7c36d9e9
Octhanos <luis.tavo@live.com>
2023-01-22 23:39:01 -0600

    First commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBvLbNTY3KD2fc6xXwPD-sfaPwCTKjffrI",
+  authDomain: "my-react-blog-af39a.firebaseapp.com",
+  projectId: "my-react-blog-af39a",
+  storageBucket: "my-react-blog-af39a.appspot.com",
+  messagingSenderId: "581342039976",
+  appId: "1:581342039976:web:f11fbb2ebe1d1e50be179d"
+};
+
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
